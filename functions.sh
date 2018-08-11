b904a0f7985d3bd6d60cda792dbf7bd178ca403b
Andrew Gingrich <ajgingrich@yahoo.com>
2018-08-11 09:28:52 -0600

    finish functions

--- functions.sh
+++ functions.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 # enter your function code here
-calculations=( ["plus"]=+ ["minus"]=- ["times"]=* ["divided"]=/ )
 
 function ENGLISH_CALC {
-  echo "$1 + $3 = $(($1 + $3))"
+  CALCULATION=''
+  ANSWER=0
+
+  if [ $2 = "plus" ]; then
+    CALCULATION='+'
+    ANSWER=$(($1 + $3))
+  elif [ $2 = "minus" ]; then
+    CALCULATION='-'
+    ANSWER=$(($1 - $3))
+  elif [ $2 = "times" ]; then
+    CALCULATION='*'
+    ANSWER=$(($1 * $3))
+  else
+    CALCULATION='/'
+    ANSWER=$(($1 / $3))
+  fi
+
+  echo "$1 $CALCULATION $3 = $ANSWER"
 }
 
 # testing code
--- test.sh
+++ test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-function adder {
-  echo "$(($1 + $2))"
-}
 
-adder 5 7
+filename="sample.sh"
+
+if [ ! -f "$filename" ]; then
+  touch "$filename"
+fi
+
+if [ -r "$filename" ]; then
+  echo "you are allowed to read $filename"
+else
+  echo "permission denied"
+fi
